96e420d5e204fa083c3927a33cba1e5bc256f9df
Mark Peters <mapeters101@earthlink.net>
2024-11-05 13:37:07 -0500

    Lesson031:  Work on lesson up to 18:30.

---
+++ lesson031.py
@@ -0,0 +1,26 @@
+print(type(1+1))
+print(type(2*3))
+print(type(4-10))
+print(type(3 ** 6))
+print(type(2/3))
+print(type(10/2))
+
+import math
+print(math.floor(3.15))
+print(math.floor(3.99999999))
+print(math.floor(-3.14))
+print(math.floor(-3.000001))
+print(math.floor(-3.0000000000001))
+print(math.floor(-3.0000000000000001))
+
+
+
+
+
+
+
+
+
+
+
+
