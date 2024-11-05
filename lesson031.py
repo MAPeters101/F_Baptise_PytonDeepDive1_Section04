4840c9fd0e9cbd2dbc0aff08de77ecd38852b302
Mark Peters <mapeters101@earthlink.net>
2024-11-05 14:19:31 -0500

    Lesson031:  Complete work on lesson from 18:30.

--- lesson031.py
+++ lesson031.py
@@ -12,12 +12,81 @@ print(math.floor(-3.14))
 print(math.floor(-3.000001))
 print(math.floor(-3.0000000000001))
 print(math.floor(-3.0000000000000001))
+print('-'*10)
 
+a = 33
+b = 16
+print(f'a: {a}  b: {b}')
+print('a/b:', a/b)
+print('a//b:', a//b)
+print('floor(a/b):', math.floor(a/b))
+print('trunc(a/b):', math.trunc(a/b))
+print('a%b:', a%b)
+print()
 
+a = -33
+b = 16
+print(f'a: {a}  b: {b}')
+print('a/b:', a/b)
+print('a//b:', a//b)
+print('floor(a/b):', math.floor(a/b))
+print('trunc(a/b):', math.trunc(a/b))
+print('a%b:', a%b)
+print()
 
+a = 33
+b = -16
+print(f'a: {a}  b: {b}')
+print('a/b:', a/b)
+print('a//b:', a//b)
+print('floor(a/b):', math.floor(a/b))
+print('trunc(a/b):', math.trunc(a/b))
+print('a%b:', a%b)
+print()
 
+a = -33
+b = -16
+print(f'a: {a}  b: {b}')
+print('a/b:', a/b)
+print('a//b:', a//b)
+print('floor(a/b):', math.floor(a/b))
+print('trunc(a/b):', math.trunc(a/b))
+print('a%b:', a%b)
+print()
 
+print('='*10)
 
+a = 13
+b = 4
+print(f'{a}/{b} = {a/b}')
+print(f'{a}//{b} = {a//b}')
+print(f'{a}%{b} = {a%b}')
+print( a == b * (a//b) + a%b)
+print()
+
+a = -13
+b = 4
+print(f'{a}/{b} = {a/b}')
+print(f'{a}//{b} = {a//b}')
+print(f'{a}%{b} = {a%b}')
+print( a == b * (a//b) + a%b)
+print()
+
+a = 13
+b = -4
+print(f'{a}/{b} = {a/b}')
+print(f'{a}//{b} = {a//b}')
+print(f'{a}%{b} = {a%b}')
+print( a == b * (a//b) + a%b)
+print()
+
+a = -13
+b = -4
+print(f'{a}/{b} = {a/b}')
+print(f'{a}//{b} = {a//b}')
+print(f'{a}%{b} = {a%b}')
+print( a == b * (a//b) + a%b)
+print()
 
 
 
