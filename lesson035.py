0f8f081367b40c98e83e8897c19ef59cbcb381c5
Mark Peters <mapeters101@earthlink.net>
2024-11-06 10:23:45 -0500

    Lesson035:  Work on lesson to 5:30.

---
+++ lesson035.py
@@ -0,0 +1,29 @@
+from fractions import Fraction
+
+#help(Fraction)
+
+print(Fraction(1))
+print(Fraction(denominator=1, numerator=2))
+print(Fraction(2, 1))
+print(Fraction(1, 2))
+print(Fraction(numerator=2, denominator=1))
+print(Fraction(0.125))
+print(Fraction('0.125'))
+print(Fraction('22/7'))
+print('='*10)
+x = Fraction(2, 3)
+y = Fraction(3, 4)
+print(x + y)
+print(x * y)
+print(x / y)
+print(Fraction(8, 16))
+print(Fraction(1, -4))
+x = Fraction(1, -4)
+print(x.numerator)
+print(x.denominator)
+print('-'*10)
+
+
+
+
+
