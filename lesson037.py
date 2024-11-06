c4b66454c31e822dc520967e33340306624e46a6
Mark Peters <mapeters101@earthlink.net>
2024-11-06 10:50:16 -0500

    Lesson037:  Work on lesson.

---
+++ lesson037.py
@@ -0,0 +1,30 @@
+#help(float)
+
+print(float(10))
+print(float(10.4))
+print(float('12.5'))
+#print(float('22/7'))
+
+from fractions import Fraction
+a = Fraction('22/7')
+print(float(a))
+
+print(0.1)
+print(format(0.1, '.15f'))
+print(format(0.1, '.25f'))
+print()
+
+print(0.125)
+print(1/8)
+print(format(0.125, '.25f'))
+
+a = 0.1 + 0.1 + 0.1
+b = 0.3
+print(a == b)
+
+print(format(a, '.25f'))
+print(format(b, '.25f'))
+
+
+
+
