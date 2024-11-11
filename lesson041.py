8ce0ee7ca8daf632a0bfff0426abe06b81c7dbeb
Mark Peters <mapeters101@earthlink.net>
2024-11-11 12:50:08 -0500

    Lesson041:  Complete work on lesson.

---
+++ lesson041.py
@@ -0,0 +1,16 @@
+from math import trunc
+
+print(trunc(10.3), trunc(10.5), trunc(10.9))
+print(trunc(-10.3), trunc(-10.5), trunc(-10.9))
+print(int(10.4), int(10.5), int(10.9))
+print(int(-10.4), int(-10.5), int(-10.9))
+print()
+
+from math import floor
+print(floor(10.3), floor(10.5), floor(10.9))
+print(floor(-10.3), floor(-10.5), floor(-10.9))
+
+from math import ceil
+print(ceil(10.4), ceil(10.5), ceil(10.9))
+print(ceil(-10.4), ceil(-10.5), ceil(-10.9))
+
