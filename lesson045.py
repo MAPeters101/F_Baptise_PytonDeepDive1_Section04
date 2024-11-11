a8b9651259b3d893d532291b48c5bba347db28fc
Mark Peters <mapeters101@earthlink.net>
2024-11-11 18:03:01 -0500

    Lesson045:  Complete work on lesson from 4:40.

--- lesson045.py
+++ lesson045.py
@@ -24,7 +24,35 @@ print(decimal.ROUND_HALF_UP)
 g_ctx.prec = 28
 g_ctx.rounding = decimal.ROUND_HALF_EVEN
 print(decimal.getcontext())
+print()
 
+print(decimal.localcontext())
+print(type(decimal.localcontext()))
+print(type(decimal.getcontext()))
+print()
+
+x = Decimal('1.25')
+y = Decimal('1.35')
+
+with decimal.localcontext() as ctx:
+    print(type(ctx))
+    print(ctx)
+    ctx.prec = 6
+    ctx.rounding = decimal.ROUND_HALF_UP
+    print(ctx)
+    print(decimal.getcontext())
+    print(id(ctx))
+    print(id(decimal.getcontext()))
+    print(id(ctx) == id(decimal.getcontext()))
+    print(round(x, 1))
+    print(round(y, 1))
+
+
+print('-+'*30)
+print(decimal.getcontext())
+print(id(decimal.getcontext()))
+print(round(x, 1))
+print(round(y, 1))
 
 
 
