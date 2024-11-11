b79687f0b2c72304a47b7eb32deb83310771629b
Mark Peters <mapeters101@earthlink.net>
2024-11-11 18:23:50 -0500

    Lesson047:  Complete work on lesson from 5:00.

--- lesson047.py
+++ lesson047.py
@@ -31,3 +31,34 @@ print(format(0.1, '.25f'))
 print(Decimal(0.1))
 print(Decimal(0.1) == Decimal('0.1'))
 print(Decimal(10) == Decimal('10'))
+print()
+print()
+
+print(decimal.getcontext())
+decimal.getcontext().prec=6
+
+a = Decimal('0.123456789')
+print(a)
+
+decimal.getcontext().prec=2
+a = Decimal('0.12345')
+b = Decimal('0.12345')
+print(a, b)
+print(0.12345 + 0.12345)
+print(a + b)
+
+decimal.getcontext().prec=6
+a = Decimal('0.12345')
+b = Decimal('0.12345')
+print(a, b)
+print(0.12345 + 0.12345)
+print(a + b)
+
+with decimal.localcontext() as ctx:
+    ctx.prec = 2
+    c = a + b
+    print('c within local context: {0}'.format(c))
+
+print('c within global context: {0}'.format(c))
+
+
