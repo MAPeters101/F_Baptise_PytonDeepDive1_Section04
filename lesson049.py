5f4e7ccd58cc183c084fa318992f936463b3e596
Mark Peters <mapeters101@earthlink.net>
2024-11-12 10:24:25 -0500

    Lesson049:  Complete work on lesson from 5:50.

--- lesson049.py
+++ lesson049.py
@@ -41,8 +41,75 @@ print()
 print('='*70)
 
 
+a = Decimal('0.1')
+print(a.ln())
+print(a.exp())
+print(a.sqrt())
+
+import math
+#print(math.ln(a))
+print(math.exp(a))
+print(math.sqrt(a))
+
+print('-'*70)
+x = 2
+x_dec = Decimal(2)
+root_float = math.sqrt(x)
+root_mixed = math.sqrt(x_dec)
+root_dec = x_dec.sqrt()
+print(format(root_float, '1.27f'))
+print(format(root_mixed, '1.27f'))
+print(format(root_dec, '1.27f'))
+print(root_dec)
+print()
+
+print(format(root_float * root_float, '1.27f'))
+print(format(root_mixed * root_mixed, '1.27f'))
+print(format(root_dec * root_dec, '1.27f'))
+print(root_dec * root_dec)
+print()
+print('-'*60)
 
 
+x = 0.01
+x_dec = Decimal(0.01)
+print(format(x, '.27f'))
+print(format(x_dec, '.27f'))
+root_float = math.sqrt(x)
+root_mixed = math.sqrt(x_dec)
+root_dec = x_dec.sqrt()
+print(format(root_float, '1.27f'))
+print(format(root_mixed, '1.27f'))
+print(format(root_dec, '1.27f'))
+print(root_dec)
+print()
+
+print(format(root_float * root_float, '1.27f'))
+print(format(root_mixed * root_mixed, '1.27f'))
+print(format(root_dec * root_dec, '1.27f'))
+print(root_dec * root_dec)
+print()
+print('-'*60)
+
+x = 0.01
+x_dec = Decimal('0.01')
+print(format(x, '.27f'))
+print(format(x_dec, '.27f'))
+root_float = math.sqrt(x)
+root_mixed = math.sqrt(x_dec)
+root_dec = x_dec.sqrt()
+print(format(root_float, '1.27f'))
+print(format(root_mixed, '1.27f'))
+print(format(root_dec, '1.27f'))
+print(root_dec)
+print()
+
+print(format(root_float * root_float, '1.27f'))
+print(format(root_mixed * root_mixed, '1.27f'))
+print(format(root_dec * root_dec, '1.27f'))
+print(root_dec * root_dec)
+print()
+print('-'*60)
 
 
 
